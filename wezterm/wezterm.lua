bb91bb690747bfc20b58e683b381ef4fb51e966b
biless <biless@qq.com>
2024-01-15 16:25:45 +0800

    feat: init project

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,14 @@
+local wezterm = require("wezterm")
+
+return {
+	font_size = 16,
+	font = wezterm.font("CaskaydiaCove Nerd Font Propo", { italic = true }),
+	color_scheme = "Catppuccin Mocha",
+
+	use_fancy_tab_bar = false,
+	hide_tab_bar_if_only_one_tab = true,
+	window_decorations = "RESIZE",
+	show_new_tab_button_in_tab_bar = false,
+	window_background_opacity = 0.7,
+	macos_window_background_blur = 90,
+}
