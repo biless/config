95c2986270cb9a03c49c9e5cf781f249326a5044
biless <biless@qq.com>
2024-01-20 18:02:09 +0800

    fix: add test framework

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -22,6 +22,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.navic" },
     { import = "lazyvim.plugins.extras.editor.symbols-outline" },
     { import = "lazyvim.plugins.extras.lsp.none-ls" },
+    { import = "lazyvim.plugins.extras.test.core" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
