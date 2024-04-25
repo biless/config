f27dcf87aab52d022fee9958ea0e58abf7e3954f
biless <biless@qq.com>
2024-04-25 09:47:10 +0800

    feat: add docment

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -6,3 +6,6 @@ vim.keymap.set("n", "<C-h>", "<Cmd>TmuxNavigateLeft<CR>", { silent = true })
 vim.keymap.set("n", "<C-j>", "<Cmd>TmuxNavigateDown<CR>", { silent = true })
 vim.keymap.set("n", "<C-k>", "<Cmd>TmuxNavigateUp<CR>", { silent = true })
 vim.keymap.set("n", "<C-l>", "<Cmd>TmuxNavigateRight<CR>", { silent = true })
+
+-- Generate comment for current line
+vim.keymap.set("n", "<Leader>og", "<Plug>(doge-generate)")
